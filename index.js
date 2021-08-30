df68ef92ec7148467488d1781958094b4f504a32
geekyAdarsh <adarshjain9628@gmail.com>
2021-08-30 23:19:22 +0530

    change port again

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.use(express.json({ extended: false }));
 
 app.use("/api", product);
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
